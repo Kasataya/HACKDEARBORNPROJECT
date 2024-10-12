849ab44c8ca62ff7a406707117142b090dfe9920
kasataya <kassemataya0@gmail.com>
2024-10-12 17:17:53 -0400

    Adding a text box component at the bottom of application so I can look into hosting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Declare a state variable to store user input
+  const [inputValue, setInputValue] = useState('');
+
+  // Function to handle input change
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,17 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* Text Box component at the bottom */}
+      <div className="text-box">
+        <input 
+          type="text" 
+          placeholder="Enter text here..." 
+          value={inputValue} 
+          onChange={handleInputChange}
+        />
+        <p>You entered: {inputValue}</p>
+      </div>
     </div>
   );
 }
