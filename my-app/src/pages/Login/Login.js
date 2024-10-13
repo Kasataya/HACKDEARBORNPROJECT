8266208e1f09467ca1b67fdc282ee171637eb327
kasataya <kassemataya0@gmail.com>
2024-10-13 03:00:28 -0400

    sfd

--- my-app/src/pages/Login/Login.js
+++ my-app/src/pages/Login/Login.js
@@ -6,32 +6,62 @@ import { auth } from '../../firebase'; // Import Firebase
 
 function App() {
   return (
+<div className= "columns">
     <div className= "wrapper">
         <form action="">
-          <h1>Login</h1>
+          <h1>User-1</h1>
+
           <div className="input-box">
-                <input type="text" placeholder='Username' required/>
-                <FaUser className='icon'/>
-          </div>
-          <div className="input-box">
-                <input type="password" placeholder='Password' required/>
-                <FaLock className='icon'/>
-          </div>
-          <div className="remember-forgot">
-                <label><input type='checkbox' />Remember me</label>
-                <a href='#'>Forgot password?</a>
+                <input type="text" placeholder='....Input' required/>
           </div>
 
-          <button type='submit'>Login</button>
 
-          <div className="register-link">
-            <p>Dont have an account? <a href='#'>Register</a></p>
-          </div>
-          <div className="register-link">
-            <p>Log in with google <a href='#'>Google</a></p>
+          <button type='submit'>Submit Prompt</button>
+
+
+
+        </form>
+    </div>
+
+<div class= "rows">
+    <div className= "wrapper">
+        <form action="">
+          <div className="input-bo">
+                <p>The debate prompt given to both users</p>
           </div>
         </form>
     </div>
+    <div className= "wrapper">
+        <form action="">
+          <h1>AI</h1>
+          <div className="input-box">
+                <input type="text" placeholder='AI JUDGING .....' required/>
+          </div>
+        </form>
+    </div>
+
+</div>
+
+
+
+    <div className= "wrapper">
+        <form action="">
+          <h1>User-2</h1>
+
+          <div className="input-box">
+                <input type="text" placeholder='....Input' required/>
+          </div>
+
+
+          <button type='submit'>Submit Prompt</button>
+
+
+
+        </form>
+    </div>
+</div>
+
+    
   );
 }
 
