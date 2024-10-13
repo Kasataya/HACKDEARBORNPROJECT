5b324137d278e9639028e4bca145421a93255e9a
MoeAbboud <mohamadabboud200@outlook.com>
2024-10-12 23:51:54 -0400

    updated js and css for Home page

--- my-app/src/pages/Home/Home.js
+++ my-app/src/pages/Home/Home.js
@@ -1,11 +1,34 @@
 // src/pages/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css'; // Import a CSS file for styling
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="home-container">
+      <header className="header">
+        <h1 className="title">JudgedByAI</h1>
+        <p className="subtitle">Debate your Friends!</p>
+      </header>
+      <main className="main-content">
+        <div className="button-container">
+          <button className="play-button">Play Now</button>
+          <button className="join-button">Create Lobby</button>
+        </div>
+        <div className="rules">
+          <h2>How to Play</h2>
+          <ul>
+            <li>Two debaters will be given a case to defend</li>
+            <li>They will be given some time to formulate they're argument</li>
+            <li>They will each be given time to Debate their argument</li>
+            <li>After each is done the Judge will give feedback and decide the winner</li>
+            <li>Have fun and compete with friends!</li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        <p>©hey there</p>
+      </footer>
     </div>
   );
 }
